1e6bb6419634438cc6c50a29a807fad262d20d75
artrsyf <rom.fadeev2017@yandex.ru>
2024-03-18 15:30:25 +0300

    refactored tasks 1-3

--- module1/task3/task3.cpp
+++ module1/task3/task3.cpp
@@ -2,25 +2,49 @@
 #include <sstream>
 #include <cassert>
 
+/*
+* 3_2. Реализовать дек с динамическим зацикленным буфером (на основе динамического массива).
+* Требования: Дек должен быть реализован в виде класса.
+*
+* in              out
+* -------------------
+* 3               YES
+* 1 44
+* 3 50
+* 2 44
+* -------------------
+* 2               YES
+* 2 -1
+* 1 10
+* -------------------
+* 2               NO
+* 3 44
+* 4 66
+*/
+
 template <typename T>
 class Deque
 {
 public:
-    Deque(); 
+    Deque();
 
-    ~Deque(){};
+    Deque( const Deque & deque );
 
-    void pushFront(T data);
+    Deque & operator=( const Deque & deque );
 
-    void pushBack(T data);
+    ~Deque();
+
+    void pushFront( T data );
+
+    void pushBack( T data );
 
     T popFront();
 
     T popBack();
 
-    void resize();
+    int getSize () const;
 
-    void display();
+    void display( std::ostream& output ) const;
 
 private:
     T * buffer;
@@ -28,48 +52,109 @@ private:
     int tail_index;
     int size;
     int capacity;
+
+    void resize();
 };
 
 template <typename T>
-Deque<T>::Deque() : head_index(1), tail_index(0), size(0), capacity(5)
+Deque<T>::Deque() : head_index( 1 ), tail_index( 0 ), size( 0 ), capacity( 5 )
 {
     buffer = new T[capacity];
 }
 
 template <typename T>
-void Deque<T>::pushBack(T data)
+Deque<T>::Deque( const Deque<T> & deque )
+{
+    this->capacity = deque.capacity;
+    this->size = deque.size;
+    this->head_index = deque.head_index;
+    this->tail_index = deque.tail_index;
+
+    this->buffer = new T[this->capacity];
+
+    for ( size_t i = 0; i < this->capacity; ++i )
+    {
+        this->buffer[i] = deque.buffer[i];
+    }
+}
+
+template <typename T>
+Deque<T> & Deque<T>::operator=( const Deque<T> & deque )
+{
+    if ( this != &deque )
+    {
+        delete[] this->buffer;
+
+        this->capacity = deque.capacity;
+        this->size = deque.size;
+        this->head_index = deque.head_index;
+        this->tail_index = deque.tail_index;
+
+        this->buffer = new T[this->capacity];
+
+        for ( size_t i = 0; i < this->capacity; ++i )
+        {
+            this->buffer[i] = deque.buffer[i];
+        }
+    }
+
+    return *this;
+}
+
+// Освобождение памяти
+template <typename T>
+Deque<T>::~Deque()
+{
+    delete[] buffer;
+}
+
+template <typename T>
+void Deque<T>::pushBack( T data )
 {
     if ( size == capacity )
         resize();
 
     head_index--;
 
-    if (head_index == -1)
+    if ( head_index == -1 )
     {
         head_index = capacity - 1;
-        // assert( tail_index != capacity - 1 );
     }
 
     buffer[head_index] = data;
-    
     size++;
 }
 
-// refactor
+// Метод для динамического увеличения размера буффера
 template <typename T>
 void Deque<T>::resize()
 {
     capacity *= 2;
     T * new_buffer = new T[capacity];
+
     bool make_once = true;
     int loop_bound = size;
+
+    // ordered_index используем, чтобы не было зацикленности
+    // в новом буффере (т.к. у него объем больше в 2 раза)
     int oredered_index = 0;
 
+    // Проходим по буфферу от индекса первого элемента
+    // до границы (в начале она равна размеру массива),
+    // как только head_index дошел до конца буффера,
+    // перемещаем его на 0 индекс и устанавливаем границу,
+    // равную tail_index. Это нужно для прохождения
+    // зацикленного буффера
     for ( int i = head_index; i < loop_bound; ++i )
     {
         new_buffer[oredered_index] = buffer[i];
         oredered_index++;
 
+        // Случай, когда head_index дошел до конца буффера.
+        // Очевидно, что для обхода буффера, нужно переместить head_index
+        // только один раз (реализует флаг make_once).
+        // i устанавливаем как -1, чтобы на следующей итерации
+        // иметь индекс 0
         if ( i == size - 1 && make_once )
         {
             i = -1;
@@ -81,22 +166,22 @@ void Deque<T>::resize()
     head_index = 0;
     tail_index = size - 1;
 
+    // Освобождение памяти после переноса старого буффера
     delete[] buffer;
     buffer = new_buffer;
 }
 
 template <typename T>
-void Deque<T>::pushFront(T data)
+void Deque<T>::pushFront( T data )
 {
     if ( size == capacity )
         resize();
 
     tail_index++;
 
-    if (tail_index == capacity)
+    if ( tail_index == capacity )
     {
         tail_index = 0;
-        // assert( head_index != 0 );
     }
     
     buffer[tail_index] = data;
@@ -120,6 +205,7 @@ T Deque<T>::popBack()
     }
 
     size--;
+
     return pop_element;
 }
 
@@ -144,13 +230,16 @@ T Deque<T>::popFront()
     return pop_element;
 }
 
+// Для дебага, выводит непрерывно элементы так,
+// как они находятся в памяти
 template <typename T>
-void Deque<T>::display()
+void Deque<T>::display( std::ostream& output ) const
 {
-    std::cout << "\nBegin and end indexes: " << head_index << " " << tail_index << "\nArray is: ";
-    for (int i = 0; i < capacity; ++i)
+    output << "\nBegin and end indexes: " << head_index << " " << tail_index << "\nArray is: ";
+
+    for ( size_t i = 0; i < capacity; ++i )
     {
-        std::cout << buffer[i] << ' ';
+        output << buffer[i] << "[address: " << buffer + i << ']' << ' ';
     }
 }
 
@@ -161,28 +250,33 @@ void run( std::istream& input, std::ostream& output )
     input >> command_count;
     bool result = true;
 
-    while (command_count--)
+    while ( command_count-- )
     {
         input >> command_number >> data;
-        int test = 0;
         switch( command_number )
         {
             case 1:
-                my_deque->pushFront(data);
+                my_deque->pushFront( data );
                 break;
             case 2:
-                result &= (my_deque->popFront() == data);
+                result &= ( my_deque->popFront() == data );
                 break;
             case 3:
-                my_deque->pushBack(data);
+                my_deque->pushBack( data );
                 break;
             case 4:
-                result &= (my_deque->popBack() == data);
+                result &= ( my_deque->popBack() == data );
                 break;
         }
     }
 
-    output << (result ? "YES" : "NO");
+    output << ( result ? "YES" : "NO" );
+}
+
+template <typename T>
+int Deque<T>::getSize () const
+{
+    return this->size();
 }
 
 void testDeque()
@@ -193,8 +287,7 @@ void testDeque()
 
         input << "3\n1 44\n3 50\n2 44";
         run( input, output );
-        std::cout << output.str() << '\n';
-        // assert( output.str() == "0 0 0 2" );
+        assert( output.str() == "YES" );
     }
     {
         std::stringstream input;
@@ -202,8 +295,7 @@ void testDeque()
 
         input << "2\n2 -1\n1 10";
         run( input, output );
-        std::cout << output.str() << '\n';
-        // assert( output.str() == "0 0 0 2" );
+        assert( output.str() == "YES" );
     }
     {
         std::stringstream input;
@@ -211,8 +303,7 @@ void testDeque()
 
         input << "2\n3 44\n4 66";
         run( input, output );
-        std::cout << output.str() << '\n';
-        // assert( output.str() == "0 0 0 2" );
+        assert( output.str() == "NO" );
     }
     {
         std::stringstream input;
@@ -220,40 +311,41 @@ void testDeque()
 
         input << "12\n3 44\n3 66\n3 12\n3 21\n3 89\n3 43\n2 44\n2 66\n2 12\n2 21\n2 89\n2 43";
         run( input, output );
-        std::cout << output.str() << '\n';
-        // assert( output.str() == "0 0 0 2" );
+        assert( output.str() == "YES" );
+    }
+    {
+        Deque<int> * first_deque = new Deque<int>();
+        first_deque->pushBack( 1 );
+        first_deque->pushBack( 22 );
+
+        Deque<int> * second_deque = new Deque<int>( * first_deque );
+
+        std::cout << "Copy constructor test: " << '\n';
+        first_deque->display(std::cout);
+        second_deque->display(std::cout);
+    }
+    {
+        Deque<int> * first_deque = new Deque<int>();
+        first_deque->pushBack( 1 );
+        first_deque->pushBack( 22 );
+
+        Deque<int> * second_deque = new Deque<int>();
+        second_deque->pushBack( 333 );
+        second_deque->pushBack( 4444 );
+
+        *second_deque = *first_deque;
+
+        std::cout << "\nOperator= test: " << '\n';
+        first_deque->display( std::cout );
+        second_deque->display( std::cout );
     }
 }
 
 int main()
 {
     // testDeque();
-    run( std::cin, std::cout );
 
-    // Deque<int> * my = new Deque<int>();
-    // // 213456
-    // my->pushBack(44);
-    // my->display();
-    // my->pushBack(66);
-    // my->display();
-    // my->pushBack(12);
-    // my->display();
-    // my->pushBack(21);
-    // my->display();
-    // my->pushBack(89);
-    // my->display();
-    // // 89 21 12 66 44
-    // my->pushBack(43);
-    // my->display();
-    // std::cout << '\n' << my->popFront() << '\n';
-    // my->display();
-    // std::cout << '\n' << my->popFront() << '\n';
-    // my->display();
-    // std::cout << '\n' << my->popFront() << '\n';
-    // my->display();
-    // std::cout << '\n' << my->popFront() << '\n';
-    // std::cout << '\n' << my->popFront() << '\n';
-    // std::cout << '\n' << my->popFront() << '\n';
+    run( std::cin, std::cout );
 
     return 0;
 }
